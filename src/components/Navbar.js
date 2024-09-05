b1c4e158259d387b51110ca4d6e179bdd57c8fc7
engineerdogan <99654850+engineerdogan@users.noreply.github.com>
2024-09-05 13:57:49 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,16 @@ import './Navbar.css';
 const Navbar = () => (
     <nav className="navbar">
         <NavLink to="/" className="nav-link" activeClassName="active">
-            <i className="fas fa-key"></i> Şifre Üret
+            <i className="fas fa-key"></i> Şifre Üret {/* Ana sayfa için bir anahtar ikonu */}
         </NavLink>
         <NavLink to="/page1" className="nav-link" activeClassName="active">
-            <i className="fas fa-file-alt"></i> Sayfa 1
+            <i className="fas fa-tools"></i> SAP {/* SAP sayfası için bir alet ikonu */}
         </NavLink>
         <NavLink to="/page2" className="nav-link" activeClassName="active">
-            <i className="fas fa-file-alt"></i> Sayfa 2
+            <i className="fas fa-gamepad"></i> Sayfa 2 {/* Oyunlar veya eğlence ile ilgili bir gamepad ikonu */}
         </NavLink>
         <NavLink to="/page3" className="nav-link" activeClassName="active">
-            <i className="fas fa-file-alt"></i> Sayfa 3
+            <i className="fas fa-clipboard-list"></i> To-Do {/* Yapılacak işler ikonu */}
         </NavLink>
     </nav>
 );
