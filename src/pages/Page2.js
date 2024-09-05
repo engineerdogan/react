5838bf9d115f36d3753d06a0481c59ce82d795e7
engineerdogan <99654850+engineerdogan@users.noreply.github.com>
2024-09-05 10:11:52 +0300

    Update Page2.js

--- src/pages/Page2.js
+++ src/pages/Page2.js
@@ -1,8 +1,53 @@
-import React from 'react';
-import './Page2.css'; // CSS dosyasını import et
+import React, { useState } from 'react';
+import './Page2.css';
 
-const Page2 = () => (
-    <div className="page2-container"></div>
-);
+const gameOptions = {
+    Games: [
+        { id: 12, imageUrl: 'https://cdn.hamsterkombat.io/fluff/image.webp', name: 'Fluff Crusade' },
+        { id: 11, imageUrl: 'https://cdn.hamsterkombat.io/zoopolis/image.webp', name: 'Zoopolis' },
+        { id: 10, imageUrl: 'https://cdn.hamsterkombat.io/tile_trio/image.webp', name: 'Tile Trio' },
+        { id: 9, imageUrl: 'https://cdn.hamsterkombat.io/mud_racing/image.webp', name: 'Mud Racing' },
+    ],
+    Minigames: [
+        { id: 8, imageUrl: 'https://cdn.hamsterkombat.io/mow_trim/image.webp', name: 'Mow and Trim' },
+        { id: 7, imageUrl: 'https://cdn.hamsterkombat.io/polysphere/image.webp', name: 'Polysphere' },
+        { id: 6, imageUrl: 'https://cdn.hamsterkombat.io/twerk/image.webp', name: 'Twerk Race 3D' },
+        { id: 5, imageUrl: 'https://cdn.hamsterkombat.io/merge_away/image.webp', name: 'Merge Away' },
+        { id: 2, imageUrl: 'https://cdn.hamsterkombat.io/chain_cube/image.webp', name: 'Chain Cube 2048' },
+    ],
+};
+
+const Page2 = () => {
+    const [activeTab, setActiveTab] = useState('Games'); // Varsayılan olarak "Games" sekmesini aktif yap
+
+    return (
+        <div className="page2-container">
+            <div className="page2-header-wrapper">
+                <div className="page2-header">
+                    <div 
+                        className={`page2-tab ${activeTab === 'Games' ? 'active' : ''}`}
+                        onClick={() => setActiveTab('Games')}
+                    >
+                        Games
+                    </div>
+                    <div 
+                        className={`page2-tab ${activeTab === 'Minigames' ? 'active' : ''}`}
+                        onClick={() => setActiveTab('Minigames')}
+                    >
+                        Minigames
+                    </div>
+                </div>
+            </div>
+            <div className="page2-grid-container">
+                {gameOptions[activeTab].map(game => (
+                    <div className="page2-game-option" key={game.id} data-game={game.id}>
+                        <img src={game.imageUrl} alt={game.name} />
+                        <p>{game.name}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default Page2;
